172aa89ea1c925576c4ac9caccd1f3078f973048
Cyrille <63170122+HealusionDev@users.noreply.github.com>
2020-04-12 00:20:45 +0200

    temp modif auth

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,15 @@
 @extends('layouts.app')
 
+@section('title', 'My Next Adventure')
+
+@section('header')
+    @parent
+@endsection
+
 @section('content')
-<div class="container">
+
+	<!-- a integrer dans app
+	<div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -19,5 +27,107 @@
             </div>
         </div>
     </div>
-</div>
+</div> -->
+
+
+    	<!-- Image Slider -->
+	<div id="slides" class="carousel slide" data-ride="carousel">
+		<ul class="carousel-indicators">
+			<li data-target="#slides" data-slide-to="0" class="active"></li>
+			<li data-target="#slides" data-slide-to="1"></li>
+			<li data-target="#slides" data-slide-to="2"></li>
+		</ul>
+		<div class="carousel-inner">
+
+			<div class="carousel-item active">
+				<img class="carouselimg d-block mx-auto" src="/img/guyane1.jpg" data-holder-rendered="true">
+					<div class="carousel-caption">
+						<h1 class="carousel-titre1">Guyane</h1>
+						<h2 class="carousel-titre2">Plongez au coeur de la forêt amazonienne</h2>
+						<button type="button" class="btn btn-primary btn-lg" onclick="window.location.href='../guide-guyane';">Lire le guide</button>
+					</div>
+			</div>
+			<div class="carousel-item">
+				<img class="carouselimg d-block mx-auto" src="/img/maroc1.jpg" data-holder-rendered="true">
+					<div class="carousel-caption">
+						<h1 class="carousel-titre1">Maroc</h1>
+						<h2 class="carousel-titre2">L'antre des milles couleurs</h2>
+						<button type="button" class="btn btn-primary btn-lg" onclick="window.location.href='../guide-maroc';">Lire le guide</button>
+					</div>
+			</div>
+			<div class="carousel-item">
+				<img class="carouselimg d-block mx-auto" src="/img/guyane1.jpg" data-holder-rendered="true">
+					<div class="carousel-caption">
+						<h1 class="carousel-titre1">TBD</h1>
+						<h2 class="carousel-titre2">TBD</h2>
+						<button type="button" class="btn btn-primary btn-lg" onclick="window.location.href='../guide-guyane';">Lire le guide</button>
+					</div>
+			</div>
+		</div>
+			<a class="carousel-control-prev" href="#slides" role="button" data-slide="prev">
+			    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			    <span class="sr-only">Previous</span>
+			</a>
+		  <a class="carousel-control-next" href="#slides" role="button" data-slide="next">
+		    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+		    <span class="sr-only">Next</span>
+		  </a>
+	</div>
+
+	<!-- Contenu Présentation -->
+	<div class="container-fluid p-0">
+		<div class="jumbotron">
+			<div class="col-12 d-flex justify-content-center">
+				<p class="lead text-center" style="width: 80%;">Parce que la nature et les bons mets de notre terroir sont des sources d'inspiration inépuisables.<br/><br/>
+				Parce que le partage d'itinéaires de trekking, d'astuces voyages, de délicieuses recettes healthy et d'expériences oenologiques sont essentielles à notre épanouissement.
+				</p>
+			</div>
+			<div class="col-12">
+				<button type="button" class="btn btn-outline-secondary btn-lg" onclick="window.location.href='about.html';">En savoir plus</button>
+			</div>
+		</div>
+	</div>
+
+	<!-- New Movies section -->
+	<div class="container-fluid p-3">
+		<div class="row moviesbox text-center">
+			<div class="col-12">
+				<h1 class="movies-title">Movies</h1>
+				<hr class="style1">
+			</div>
+			
+			<div class="col-12 d-flex justify-content-center">
+				<p class="lead text-center" style="width: 70%;">
+					La dernière expédition en date remonte à l'année 2019 lors de mon voyage en Guyane.<br/>
+					Pour un contenu vidéo il faudra revenir à l'aventure Australienne en 2017.<br/>
+				</p>
+			</div>
+			<div class="col-12 d-flex justify-content-center"> 
+				<iframe width="560" height="315" src="https://www.youtube.com/embed/59QYI9U9I2s" frameborder="0" samesite="none" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+			</div>
+		</div>
+	</div>
+	
+	<!-- Connect -->
+	<div class="container-fluid pt-5">
+		<div class="row connect text-center">
+			<div class="col-12">
+				<h2 class="connect-title">Connect</h2>
+				<hr class="style2">
+			</div>
+			<div class="col-12 social p-1.5">
+				<a class="social-link" href="https://www.facebook.com/cyrille.gayraud.0"><i class="fab fa-facebook"></i></a>
+				<a class="social-link" href="https://www.instagram.com/cyrgrd/"><i class="fab fa-instagram"></i></a>
+				<a class="social-link" href="https://www.youtube.com/channel/UCJ6nesNo0936uP7NhfiI96w?view_as=public"><i class="fab fa-youtube"></i></a>
+			</div>
+		</div>
+	</div>
+@endsection
+
+@section('footer')
+    @parent
+@endsection
+
+@section('content')
+
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,80 +1,164 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <head>
+        <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">  
+        <!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
+        <script src="{{ mix('/js/app.js') }}"></script>
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="utf-8">
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+        <!-- <title>{{ config('app.name', 'Laravel') }}</title> -->
+   
+        <title>@yield('title')</title>
+    </head>
+    <body>
+        @section('header')
+                <div id="app">
+                    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+                        <div class="container">
+                            <a class="navbar-brand" href="{{ url('/') }}">
+                                {{ config('app.name', 'Laravel') }}
+                            </a>
+                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                                <!-- Left Side Of Navbar -->
+                                <ul class="navbar-nav mr-auto">
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+                                </ul>
 
-                    </ul>
+                                <!-- Right Side Of Navbar -->
+                                <ul class="navbar-nav ml-auto">
+                                    <!-- Authentication Links -->
+                                    @guest
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                        </li>
+                                        @if (Route::has('register'))
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                            </li>
+                                        @endif
+                                    @else
+                                        <li class="nav-item dropdown">
+                                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                                {{ Auth::user()->name }} <span class="caret"></span>
+                                            </a>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                                   onclick="event.preventDefault();
+                                                                 document.getElementById('logout-form').submit();">
+                                                    {{ __('Logout') }}
+                                                </a>
+
+                                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                                    @csrf
+                                                </form>
+                                    </div>
                                 </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
+                            @endguest
+                        </ul>
+                    </div>
+                </div>
+            </nav>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+    
+            <!-- Barre de navigation -->
+            <nav class="navbar navbar-expand-md navbar-light bg-light p-0">
+                
+                <div class="form-row align-items-center my-1">
+                    <div class="col-md-4 pl-3 pr-3">
+                        <button type="button" class="btn btn-outline-secondary" onclick="window.location.href='../user/connect';">S'identifier</button>
+                    </div>
+                    <div class="col-md-8 pl-4">
+                        <button type="button" class="btn btn-outline-secondary" onclick="window.location.href='../user/new';">Rejoignez-nous</button>
+                    </div>
+                </div>
+                
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="../home">Accueil</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="../team">Equipe</a>
+                    </li>
+                    <li class="nav-item active">
+                            <a class="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+            </nav>
+
+            <!-- Menu déroulant-->
+            
+             <!--   <img class="imgheader" src="/img/header5bis.png"> -->
+        
+
+            <div class="text-menu text-center d-flex align-items-end justify-content-center p-0" style="height: 14rem;">
+                <div class="dropdown">
+                    <li class="mainbutton-menu dropdown-toggle">Gastronomie</li>
+                        <div class="dropdown-child">
+                            <a class="dropdown-child-link" href="../recettes">Recettes</a>
+                            <div class="dropdown-divider m-0"></div>
+                            <a class="dropdown-child-link" href="../vin">Vin</a>
+                            </div>
+                        </div>
+                <div class="dropdown">
+                    <li class="mainbutton-menu dropdown-toggle">Guide Voyage</li>
+                        <div class="dropdown-child">
+                            <a class="dropdown-child-link" href="../guide-maroc">Maroc</a>
+                                <div class="dropdown-divider m-0"></div>
+                                <a class="dropdown-child-link" href="../guide-guyane">Guyane</a>
+                                <div class="dropdown-divider m-0"></div>
+                                <a class="dropdown-child-link" href="../guide-australie">Australie</a>
                                 </div>
-                            </li>
-                        @endguest
-                    </ul>
+                        </div>
+                <div class="dropdown">
+                    <li class="mainbutton-menu dropdown-toggle">Expéditions</li>
+                        <div class="dropdown-child" style="width: text-size-adjust">
+                                <a class="dropdown-child-link" href="../corse-gr20">Corse : GR20 </a>
+                                <div class="dropdown-divider m-0"></div>
+                                <a class="dropdown-child-link"  href="../expedition-7summits">Seven Summits</a>
+                                <div class="dropdown-divider m-0"></div>
+                                <a class="dropdown-child-link"  href="../conseil-materiel">Matériel</a>
+                                </div>
+                        </div>
+                <div class="dropdown">
+                    <li class="mainbutton-menu dropdown-toggle">Randonnées</li>
+                        <div class="dropdown-child">
+                                <a class="dropdown-child-link" href="../rando-idf">Ile de France</a>
+                                <div class="dropdown-divider m-0"></div>
+                                <a class="dropdown-child-link" href="../rando-pyrenees">Pyrénées</a>
+                
+                        </div>
                 </div>
             </div>
-        </nav>
+        @show
 
-        <main class="py-4">
+        <div class="container-fluid p-0">
             @yield('content')
-        </main>
-    </div>
-</body>
+        </div>
+
+        @section('footer')
+        <!-- Pied de page -->
+        <footer>
+            <div class="container-fluid">
+                <div class="boxfooter text-center">
+                    <div class="col-12">
+                        <h5>&copy; nextadventure.fr</h5>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        @show
+
+    </body>
 </html>
