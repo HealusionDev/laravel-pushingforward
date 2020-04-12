777224fd49fdb23c3555d05912df84ee21454ea4
Cyrille <63170122+HealusionDev@users.noreply.github.com>
2020-04-12 13:23:00 +0200

    DB advance 2

--- routes/web.php
+++ routes/web.php
@@ -77,4 +77,6 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::resource('/admin/user','Admin\UsersController', ['except' => ['show', 'create', 'store']]);
+Route::namespace('Admin')->prefix('admin')->name('admin.')->group(function(){
+    Route::resource('/users','UsersController', ['except' => ['show', 'create', 'store']]);
+});
