2258fba50e87cb55806dbf4b49107ac3306982e2
Cyrille <63170122+HealusionDev@users.noreply.github.com>
2020-04-13 23:03:29 +0200

    database edit

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -6,7 +6,7 @@
                 <div class="row justify-content-center">
                     <div class="col-md-8">
                         <div class="card">
-                            <div class="card-header">Edit user : {{ $users->name }}</div>
+                          <div class="card-header">Edit user : {{ $users->name }}</div>
                               <div class="card-body">
                                   <form action="{{ route('admin.users.update', $users) }}" method="POST">
                                     @csrf
@@ -14,8 +14,8 @@
 
                                     @foreach($roles as $role)
                                       <div class="form-check">
-                                        <input type="checkbox" name="roles[]" value="{{ $role->id }}">
-                                        <label>{{ $role->name }}</label>
+                                        <input class="form-check-input" type="checkbox" name="roles[]" value="{{ $role->id }}">
+                                        <label class="form-check-label" >{{ $role->name }}</label>
                                       </div>
                                     @endforeach
                                     <button type="submit" class="btn btn-primary">Update</button> 
@@ -25,5 +25,4 @@
                         </div>
                     </div>
                 </div>
-            </div>
 @endsection
