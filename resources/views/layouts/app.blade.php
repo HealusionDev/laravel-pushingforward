f1e973d2017973a6228780f983d76469a0e5a06d
Cyrille <63170122+HealusionDev@users.noreply.github.com>
2020-04-13 22:00:36 +0200

    database edit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
 
     <body>                   
         <!-- Barre de navigation -->
-        <div id="app">
+        <div id="app" style="min-height: 100vh; display: flex; flex-direction: column; flex: 1; flex-grow: initial;">
             <nav class="navbar navbar-expand-md navbar-light bg-light p-0">
                 <div class="container">             
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
