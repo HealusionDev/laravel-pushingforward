633cef1f9948faa26c7431b1d79c4fbfabb3a64d
Cyrille <63170122+HealusionDev@users.noreply.github.com>
2020-04-14 20:25:16 +0200

    databse update 1404

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -8,7 +8,7 @@
                         <div class="card">
                           <div class="card-header">Edit user : {{ $user->name }}</div>
                               <div class="card-body">
-                                  <form action="{{ route('admin.users.update', $user) }}" method="POST">
+                                  <form action="{{ route('admin.users.update', $users) }}" method="POST">
                                     {{ csrf_field() }}
                                     {{ method_field('PUT') }}
 
