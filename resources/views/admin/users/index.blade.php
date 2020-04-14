d08b4178ae5be599839840ba40d1b082561feed0
Cyrille <63170122+HealusionDev@users.noreply.github.com>
2020-04-14 22:25:07 +0200

    database update 1404

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -6,7 +6,7 @@
                 <div class="row justify-content-center">
                     <div class="col-md-8">
                         <div class="card">
-                            <div class="card-header">Users : {{ $users->name }}</div>
+                            <div class="card-header">Users : {{ $user->name }}</div>
 
                             <div class="card-body">
                                     <table class="table">
