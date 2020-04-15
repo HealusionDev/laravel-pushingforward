b302ae58067a3ea85bdbeb0f5658f5abd9c69b40
Cyrille <63170122+HealusionDev@users.noreply.github.com>
2020-04-15 13:11:10 +0200

    database update 1504

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -31,7 +31,7 @@
                                                     <form action="{{ route('admin.users.destroy', $user) }}" method="POST">
                                                         @csrf
                                                         {{ method_field('DELETE') }}
-                                                        <button type="submit" class="btn btn-danger">Delete</button></a>
+                                                        <button type="submit" class="btn btn-danger float-left">Delete</button></a>
                                                     </form>
                                                 </td>
                                             </tr>
