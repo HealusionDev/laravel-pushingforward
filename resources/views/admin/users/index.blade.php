4bf20973f6765334dcba677f483e8cc3c5e28ce3
Cyrille <63170122+HealusionDev@users.noreply.github.com>
2020-04-15 12:56:59 +0200

    database update 1504

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -27,11 +27,11 @@
                                                 <td>{{ $user->email }}</td>
                                                 <td>{{ implode(', ', $user->roles()->pluck('name')->toArray()) }}</td>
                                                 <td>
-                                                    <a href="{{ route('admin.users.edit', $user->id) }}"><button type="button" class="btn btn-primary float-left">Edit</button></a>
+                                                    <a href="{{ route('admin.users.edit', $user->id) }}"><button type="button" class="btn btn-primary">Edit</button></a>
                                                     <form action="{{ route('admin.users.destroy', $user) }}" method="POST">
                                                         @csrf
                                                         {{ method_field('DELETE') }}
-                                                        <button type="submit" class="btn btn-danger" class="float-left">Delete</button></a>
+                                                        <button type="submit" class="btn btn-danger">Delete</button></a>
                                                     </form>
                                                 </td>
                                             </tr>
