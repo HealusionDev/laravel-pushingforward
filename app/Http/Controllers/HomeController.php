fe67b727c9da4efd7d0e5525101db1497032b099
Cyrille <63170122+HealusionDev@users.noreply.github.com>
2020-04-18 20:54:04 +0200

    notification system

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -21,11 +21,11 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index(Request $request)
+    public function index(Request $request, User $user)
     {
 
         $request->session()->flash('sucess', $user->name . ' has been updated');
-        
+
         return view('home');
     }
 }
