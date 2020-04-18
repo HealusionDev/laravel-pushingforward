e459663dd40b27c96d3cbc1420eae12049f9c126
Cyrille <63170122+HealusionDev@users.noreply.github.com>
2020-04-18 20:37:50 +0200

    notification system

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -2,7 +2,6 @@
 
 @section('content')
             <div class="container py-4">
-              @include('partials.alerts')
                 <div class="row justify-content-center">
                     <div class="col-md-8">
                         <div class="card">
