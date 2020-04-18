6a6681ba546df0d5a6a854196310fa2f176cc3c9
Cyrille <63170122+HealusionDev@users.noreply.github.com>
2020-04-18 17:39:00 +0200

    notification system

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -64,6 +64,8 @@ class UsersController extends Controller
         $user->email = $request->email;
         $user->save();
 
+        $request->session()->flash('sucess', $user->name . ' has been updated');
+        
         if ($user->save()){
             $request->session()->flash('sucess', $user->name . ' has been updated');
         }else{
