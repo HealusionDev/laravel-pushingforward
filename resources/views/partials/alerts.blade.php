a0a8cd421616d0f0680db56ebb84b2858a8b7bb7
Cyrille <63170122+HealusionDev@users.noreply.github.com>
2020-04-19 14:09:12 +0200

    notification system

--- resources/views/partials/alerts.blade.php
+++ resources/views/partials/alerts.blade.php
@@ -1,5 +1,5 @@
 @if(session('success'))
-	<div class="alert alert-success" role="alert">
+	<div class="alert alert-success" role="alert" style="text-align: center;">
 	  {{ session('success') }}
 	</div>
 @endif
