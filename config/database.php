e808d2510af6730cc59ff3e0abde10ae09183a40
Cyrille <63170122+HealusionDev@users.noreply.github.com>
2020-05-13 14:16:27 +0200

    modif front

--- config/database.php
+++ config/database.php
@@ -192,7 +192,7 @@ return [
           * The disk names on which the backups will be stored.
           */
          'disks' => [
-             'local',
+             'Disque local',
          ],
      ],
 ]
