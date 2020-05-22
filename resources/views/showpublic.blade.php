bc8cb5ee35e79221563767e04dd2ba2dbb7eb398
HealusionDev <cyrillegayraud@hotmail.fr>
2020-05-22 23:47:31 +0200

    article update

--- resources/views/showpublic.blade.php
+++ resources/views/showpublic.blade.php
@@ -30,15 +30,15 @@
             </div>
         </div>
         <div class="col-md-7 pt-3 mr-auto" style="background-color: #f1f1f1">
-            <div class="card-body p-0 m-0">
+            <div class="card-body p-0 m-0" style="position: relative;">
                 {!! $article->detail !!}
-                <div id="title-cayenne" onclick="window.location.hash='back1'; " style="position: absolute; top: 0vh; width: 20vw; height: 2vh;"></div>
-                <div id="title-remire" onclick="window.location.hash='back2'; " style="position: absolute; top: 266vh; width: 20vw; height: 2vh;"></div>
-                <div id="title-matoury" onclick="window.location.hash='back3'; " style="position: absolute; top: 619vh; width: 20vw; height: 2vh;"></div>
-                <div id="title-routekaw" onclick="window.location.hash='back4'; " style="position: absolute; top: 656vh; width: 20vw; height: 2vh;"></div>
-                <div id="title-maraiskaw" onclick="window.location.hash='back5'; " style="position: absolute; top: 869vh; width: 20vw; height: 2vh;"></div>
-                <div id="title-routemontsinery" onclick="window.location.hash='back6'; " style="position: absolute; top: 966vh; width: 20vw; height: 2vh;"></div>
-                <div id="title-macouria" onclick="window.location.hash='back7'; " style="position: absolute; top: 1164vh; width: 20vw; height: 2vh;"></div>
+                <div id="title-cayenne" onclick="window.location.hash='back1';"></div>
+                <div id="title-remire" onclick="window.location.hash='back2';"></div>
+                <div id="title-matoury" onclick="window.location.hash='back3';"></div>
+                <div id="title-routekaw" onclick="window.location.hash='back4';"></div>
+                <div id="title-maraiskaw" onclick="window.location.hash='back5';"></div>
+                <div id="title-routemontsinery" onclick="window.location.hash='back6';"></div>
+                <div id="title-macouria" onclick="window.location.hash='back7';"></div>
             </div>
         </div>
     </div>
