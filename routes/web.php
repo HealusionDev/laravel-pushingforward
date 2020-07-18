614a09e2464cca958987e17df4bd377566d55148
HealusionDev <cyrillegayraud@hotmail.fr>
2020-07-18 14:55:07 +0200

    rtk1

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('', function () { #requete http avec address se terminant par /
+Route::get('/', function () { #requete http avec address se terminant par /
     return view('home');
 })->name('accueil');
 
