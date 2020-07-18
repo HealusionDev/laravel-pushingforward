c3e6c6ab4e3e9fd14d6ac4d97ef5a5ba86d4a171
HealusionDev <cyrillegayraud@hotmail.fr>
2020-07-18 15:11:36 +0200

    rtk1

--- resources/js/header.js
+++ resources/js/header.js
@@ -9,8 +9,23 @@ import $ from 'jquery';
 import 'bootstrap';
 
 $(document).ready(function(){
-   	if ((document.location.href.indexOf('/') > -1) || (document.location.href.indexOf('/accueil') > -1)){
+   	if (document.location.href.indexOf('/accueil') > -1){
    		document.getElementById("text-menu").style.visibility="visible";
+        document.getElementById("text-menu").style.position="absolute";
+        document.getElementById("text-menu").style.left="50%";
+        document.getElementById("text-menu").style.transform="translate(-50%)";
+        document.getElementById("text-menu").style.top="6vh";
+        document.getElementById("text-menu").style.zIndex="10";
+        document.getElementById("mainbutton-menu1").style.color="#fff";
+        document.getElementById("mainbutton-menu1").style.textShadow="1px 3px 15px #000";
+        document.getElementById("mainbutton-menu2").style.color="#fff";
+        document.getElementById("mainbutton-menu2").style.textShadow="1px 3px 15px #000";
+        document.getElementById("mainbutton-menu3").style.color="#fff";
+        document.getElementById("mainbutton-menu3").style.textShadow="1px 3px 15px #000";
+        document.getElementById("mainbutton-menu4").style.color="#fff";
+        document.getElementById("mainbutton-menu4").style.textShadow="1px 3px 15px #000";
+    }else if (document.location.href.indexOf('/') > -1){
+        document.getElementById("text-menu").style.visibility="visible";
         document.getElementById("text-menu").style.position="absolute";
         document.getElementById("text-menu").style.left="50%";
         document.getElementById("text-menu").style.transform="translate(-50%)";
