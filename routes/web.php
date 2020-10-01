9a43e59f25496ec284614c6003dba026d564b26c
HealusionDev <cyrillegayraud@hotmail.fr>
2020-10-01 11:45:22 +0200

    episode 6 + fix url

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('', function () { #requete http avec address se terminant par /
+    return view('accueil');
+})->name('accueil');
 
 Route::get('/', function () { #requete http avec address se terminant par /
     return view('accueil');
