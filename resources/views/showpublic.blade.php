efc54cf8d39728ed0a6aa5174e1aab24b2f1f565
HealusionDev <cyrillegayraud@hotmail.fr>
2020-10-26 10:42:10 +0100

    update content

--- resources/views/showpublic.blade.php
+++ resources/views/showpublic.blade.php
@@ -80,6 +80,7 @@
                         <a href="#title-rtk4" id="back1" style="text-decoration: none;"><li class="list-group-item">Episode 4</li></a>
                         <a href="#title-rtk5" id="back1" style="text-decoration: none;"><li class="list-group-item">Episode 5</li></a>
                         <a href="#title-rtk6" id="back1" style="text-decoration: none;"><li class="list-group-item">Episode 6</li></a>
+                        <a href="#title-rtk7" id="back1" style="text-decoration: none;"><li class="list-group-item">Episode 7</li></a>
                     </ul>
                 </div>
             </div>
